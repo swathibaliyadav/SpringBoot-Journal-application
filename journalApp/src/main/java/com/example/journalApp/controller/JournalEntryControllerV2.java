9a92a7e81860406e87d6cca354a6736aed5c6e09
swathibaliyadav <swathibaliyadav@gmail.com>
2024-12-04 17:18:05 -0800

    Refactoring by adding Project Lombok instead of getter and Setter

--- journalApp/src/main/java/com/example/journalApp/controller/JournalEntryControllerV2.java
+++ journalApp/src/main/java/com/example/journalApp/controller/JournalEntryControllerV2.java
@@ -5,7 +5,6 @@ import com.example.journalApp.service.JournalEntryService;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
